fc995e47343f924855cbf4f32dcef94273f5af68
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-03 15:48:56 +0800

    Update PreFetcher.scala

--- src/main/scala/madd/PreFetcher.scala
+++ src/main/scala/madd/PreFetcher.scala
@@ -1,6 +1,5 @@
 import chisel3._
 import chisel3.util._
-
 class StridePrefetcher(val addressWidth: Int, val pcWidth: Int) extends Module {
   val io = IO(new Bundle {
     // 输入端口
@@ -10,9 +9,11 @@ class StridePrefetcher(val addressWidth: Int, val pcWidth: Int) extends Module {
     val prefetch_address = Output(UInt(addressWidth.W))
     val prefetch_valid = Output(Bool())
   })
-
   // 实现Stride Prefetcher的逻辑，例如查找表格、步幅计算和预取地址生成等
-
+  val file=Mem(1024,Mem(3,0.UInt(pcWidth.W)))
+  file(io.pc)(0)=io.pc
+  file(io.pc)(1)=io.address
+  file(io.pc)(2)=io.address-file(io.pc-1)(1)
   // ...
   
   // 是否预取的逻辑
