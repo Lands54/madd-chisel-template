583ffee8f29ae73e0ae85c0768ea3db275aa51ac
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-10 22:30:04 +0800

    Update and rename PreFetcher.scala to Prefetcher.scala

---
+++ src/main/scala/madd/Prefetcher.scala
@@ -0,0 +1,35 @@
+import chisel3._
+import chisel3.util._
+class list extends(val addressWidth: Int, val pcWidth: Int) extends Bundle{
+  val PCS = UInt(pcWidth.W)
+  val ADS = UInt(addressWidth.W)
+  val PDS = UInt(addressWidth.W)
+}
+class StridePrefetcher(val addressWidth: Int, val pcWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val pc = Input(UInt(pcWidth.W))
+    val address = Input(UInt(addressWidth.W))
+    val prefetch_address = Output(UInt(addressWidth.W))
+    val prefetch_valid = Output(Bool())
+  })
+
+  val count = RegInit(0.U(32.W))
+  when(count >= 10240.U) {
+    count := count % 10240.U
+  }
+
+  val file = Reg(Vec(10240,new list(addressWidth,pcWidth)))
+  file(count).PCS := io.pc
+  file(count).ADS := io.address
+  file(count).PDS := 0.U
+  when(count > 0.U) {
+    file(count).PDS := file(count).ADS - file(count - 1.U).ADS
+    when(file(count).PDS == file(count - 1.U).PDS) {
+      io.prefetch_address := file(count).ADS + file(count).PDS
+      io.prefetch_valid := true.B
+    }.otherwise {
+      io.prefetch_valid := false.B
+    }
+  }
+  count := count + 1.U
+}
