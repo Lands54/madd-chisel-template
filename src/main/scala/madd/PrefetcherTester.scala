5f97a52cf4a236448069ca434c1f6bb2c78c39c5
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-11 12:47:02 +0800

    Update PrefetcherTester.scala

--- src/main/scala/madd/PrefetcherTester.scala
+++ src/main/scala/madd/PrefetcherTester.scala
@@ -15,12 +15,12 @@ class PrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
   if(peek(dut.io.prefetch_address)==i+4.U){
     acc := acc + 1.U
   }
-  if(peek(dut.io.prefetch_valid)==BigInt(i+4.U)){
+  if(peek(dut.io.prefetch_valid)==i+4.U){
     ful := ful + 1.U
   }
   }
-  println("0,4,8..ACCURATE%.f%",acc.toDouble / 256.0)
-  println("0,4,8..FULLRATE%.f%",ful.toDouble / 256.0)
+  println("0,4,8..ACCURATE%.f%",acc*1.0/256.0)
+  println("0,4,8..FULLRATE%.f%",ful*1.0/256.0)
   acc := 0.U
   ful := 0.U
   for(i <-0.U to 1024.U by 8.U)
@@ -28,15 +28,15 @@ class PrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
   poke(dut.io.pc, i)
   poke(dut.io.address,i)
   step(1)
-  when(peek(dut.io.prefetch_address)==i+8.U){
+  if(peek(dut.io.prefetch_address)==i+8.U){
     acc := acc + 1.U
   }
-  when(peek(dut.io.prefetch_valid)==1.U){
+  if(peek(dut.io.prefetch_valid)==1.U){
     ful := ful + 1.U
   }
   }
-  println("0,8,16..ACCURATE%.f%",acc.toDouble / 128.0)
-  println("0,8,16..FULLRATE%.f%",ful.toDouble / 128.0)
+  println("0,8,16..ACCURATE%.f%",acc*1.0/128.0)
+  println("0,8,16..FULLRATE%.f%",ful*1.0/128.0)
 }
 
 object PrefetcherTester extends App {
