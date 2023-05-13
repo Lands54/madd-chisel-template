271b4ed5a82b04b3efa24cf51cce39fd2b91d392
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-13 14:50:50 +0800

    Update StridePrefetcherTester.scala

--- src/main/scala/madd/StridePrefetcherTester.scala
+++ src/main/scala/madd/StridePrefetcherTester.scala
@@ -12,10 +12,10 @@ class StridePrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
   poke(dut.io.pc, i)
   poke(dut.io.address,i)
   println("%d".format(peek(dut.io.prefetch_address)))
-  println("%d".format(peek(dut.io.prefetch_vaild)))
+  println("%d".format(peek(dut.io.prefetch_valid)))
   step(1)
   println("%1-d".format(peek(dut.io.prefetch_address)))
-  println("%1-d".format(peek(dut.io.prefetch_vaild)))
+  println("%1-d".format(peek(dut.io.prefetch_valid)))
   if(peek(dut.io.prefetch_address)==i+(4.U).litValue()){
     acc = acc + 1
   }
