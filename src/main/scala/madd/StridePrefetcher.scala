16b56923c30d2ac4116bf54a17dab6727b93624f
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-13 16:33:05 +0800

    Update StridePrefetcher.scala

--- src/main/scala/madd/StridePrefetcher.scala
+++ src/main/scala/madd/StridePrefetcher.scala
@@ -9,7 +9,7 @@ class StridePrefetcher(val addressWidth: Int, val pcWidth: Int) extends Module {
     val pc = Input(UInt(pcWidth.W))
     val address = Input(UInt(addressWidth.W))
     val prefetch_address = Output(UInt(addressWidth.W))
-    val prefetch_valid = Output(UInt(10.W))
+    val prefetch_valid = Output(UInt(1.W))
   })
 class list(val addressWidth: Int, val pcWidth: Int) extends Bundle{
   var PCS = UInt(pcWidth.W)
@@ -20,23 +20,25 @@ class list(val addressWidth: Int, val pcWidth: Int) extends Bundle{
   when(count >= 1024.U) {
     count := 0.U
   }
-
-  var file = Reg(Vec(1024,new list(addressWidth,pcWidth)))
-  file(count).PCS := io.pc
-  file(count).ADS := io.address
-  file(count).PDS := 0.U
+  var file = Mem(1024,new list(addressWidth,pcWidth))
+  val newData = new list(addressWidth, pcWidth)
+  newData.PCS := io.pc
+  newData.ADS := io.address
+  newData.PDS := 0.U
+  file.update(count,newDate)
   when(count > 0.U) {
-    file(count).PDS := file(count).ADS - file(count - 1.U).ADS
-    when(file(count).PDS === file(count - 1.U).PDS) {
-      io.prefetch_address := file(count).ADS + file(count).PDS
-      io.prefetch_valid := file(count).ADS
+    newDate.PDS := file.read(count).ADS - file.read(count - 1.U).ADS
+    file.update(count,newDate)
+    when(file.read(count).PDS === file.read(count - 1.U).PDS) {
+      io.prefetch_address := file.read(count).ADS + file.read(count).PDS
+      io.prefetch_valid := 1.U
     }.otherwise {
       io.prefetch_address := file(count).ADS + file(count).PDS
-      io.prefetch_valid := file(count).ADS
+      io.prefetch_valid := 0.U
     }
   }.otherwise {
     io.prefetch_address := 4.U
-    io.prefetch_valid := file(count).ADS
+    io.prefetch_valid := 1.U
   }
   count := count + 1.U
 }
