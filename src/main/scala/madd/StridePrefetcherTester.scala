c034c28645189d445f44a46aba1f79770f7d87eb
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-13 10:30:54 +0800

    Update StridePrefetcherTester.scala

--- src/main/scala/madd/StridePrefetcherTester.scala
+++ src/main/scala/madd/StridePrefetcherTester.scala
@@ -37,7 +37,7 @@ class StridePrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
   }
   println("0,8,16..ACCURATE is %f%".format(acc.toDouble/128.0))
   println("0,8,16..FULLRATE is %f%".format(ful.toDouble/128.0))
-
+}
 class StridePrefetcherSpec extends ChiselFlatSpec {
   private val addressWidth = 32
   private val pcWidth = 32
