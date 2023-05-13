3fa28b808c3ec6a1811b8149e6b2501512e5d27b
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-13 13:31:26 +0800

    Update StridePrefetcherTester.scala

--- src/main/scala/madd/StridePrefetcherTester.scala
+++ src/main/scala/madd/StridePrefetcherTester.scala
@@ -19,8 +19,8 @@ class StridePrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
     ful := ful + 1.U
   }
   }
-  println("0,4,8..ACCURATE is %f%".format(acc.toDouble/256.0))
-  println("0,4,8..FULLRATE is %f%".format(ful.toDouble/256.0))
+  println("0,4,8..ACCURATE is %f%%".format(acc.toDouble/256.0))
+  println("0,4,8..FULLRATE is %f%%".format(ful.toDouble/256.0))
   var acct = 0.U
   var fult = 0.U
   for(i <-0.U to 1024.U by 8.U)
@@ -35,8 +35,8 @@ class StridePrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
     fult := fult + 1.U
   }
   }
-  println("0,8,16..ACCURATE is %f%".format(acct.toDouble/128.0))
-  println("0,8,16..FULLRATE is %f%".format(fult.toDouble/128.0))
+  println("0,8,16..ACCURATE is %f%%".format(acct.toDouble/128.0))
+  println("0,8,16..FULLRATE is %f%%".format(fult.toDouble/128.0))
 }
 class StridePrefetcherSpec extends ChiselFlatSpec {
   private val addressWidth = 32
