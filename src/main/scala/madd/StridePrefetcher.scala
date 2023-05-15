8e5e2bd4a0f8b8611fc3fe904b4176cd80c630a8
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-15 18:38:12 +0800

    Update StridePrefetcher.scala

--- src/main/scala/madd/StridePrefetcher.scala
+++ src/main/scala/madd/StridePrefetcher.scala
@@ -26,7 +26,7 @@ class StridePrefetcher(val addressWidth: Int, val pcWidth: Int) extends Module {
   val data_in = Wire(new List)
   data_in.ADS := io.address
   data_in.PCS := io.pc
-  val file = RegInit(VecInit(Seq.fill(1024)Wire(new List)))
+  val file = Vec(1024, new ListBundle(pcWidth, addressWidth))
 
   file(count).PCS := io.pc
   file(count).ADS := io.address
