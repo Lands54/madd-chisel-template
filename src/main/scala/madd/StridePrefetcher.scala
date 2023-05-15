c8bcb6aa1474be04de48cc89dc8bbef196b18fa1
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-15 17:21:09 +0800

    Update StridePrefetcher.scala

--- src/main/scala/madd/StridePrefetcher.scala
+++ src/main/scala/madd/StridePrefetcher.scala
@@ -20,7 +20,7 @@ class list(val addressWidth: Int, val pcWidth: Int) extends Bundle{
   when(count >= 1024.U) {
     count := 0.U
   }
-  var data_in = RegInit(new list(addressWidth,pcWidth))
+  var data_in = RegInit(Wire(new list(addressWidth,pcWidth)))
   data_in.ADS <= io.address
   data_in.PCS <= io.pc
   var file = Reg(Vec(1024,new list(addressWidth,pcWidth)))
