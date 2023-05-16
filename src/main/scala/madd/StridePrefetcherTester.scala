b5eeb4065b9bda5bf3ba9f72a8f4632bb56fc918
Lands54 <132423280+Lands54@users.noreply.github.com>
2023-05-16 22:27:02 +0800

    Update StridePrefetcherTester.scala

--- src/main/scala/madd/StridePrefetcherTester.scala
+++ src/main/scala/madd/StridePrefetcherTester.scala
@@ -11,15 +11,15 @@ class StridePrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
   {
   poke(dut.io.pc,i)
   poke(dut.io.address,i)
-  if(peek(dut.io.prefetch_address)==i+4){
-    acc = acc + 1
-  }
   if(peek(dut.io.prefetch_valid)==1){
     ful = ful + 1
+    if(peek(dut.io.prefetch_address)==i+4){
+    acc = acc + 1
+  }
   }
   step(1)
   }
-  println("0,4,8..ACCURATE is %f%%".format((acc.toDouble/256.0)*100))
+  println("0,4,8..ACCURATE is %f%%".format((acc.toDouble/ful.toDouble)*100))
   println("0,4,8..FULLRATE is %f%%".format((ful.toDouble/256.0)*100))
   var acct = BigInt(0)
   var fult = BigInt(0)
@@ -27,15 +27,16 @@ class StridePrefetcherTester(dut:StridePrefetcher)extends PeekPokeTester(dut) {
   {
   poke(dut.io.pc, i)
   poke(dut.io.address,i)
-  if(peek(dut.io.prefetch_address)==i+8){
-    acct = acct + 1
-  }
+
   if(peek(dut.io.prefetch_valid)==1){
     fult = fult + 1
+    if(peek(dut.io.prefetch_address)==i+8){
+    acct = acct + 1
+  }
   }
   step(1)
   }
-  println("0,8,16..ACCURATE is %f%%".format((acct.toDouble/128.0)*100))
+  println("0,8,16..ACCURATE is %f%%".format((acct.toDouble/fult.toDouble)*100))
   println("0,8,16..FULLRATE is %f%%".format((fult.toDouble/128.0)*100))
 }
 class StridePrefetcherSpec extends ChiselFlatSpec {
